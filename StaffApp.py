9d67528c3427e19843e56b624813fad0524be9cb
JWkahwai <103020993+JWkahwai@users.noreply.github.com>
2022-12-31 05:07:52 +0800

    Update StaffApp.py

--- StaffApp.py
+++ StaffApp.py
@@ -39,8 +39,7 @@ def home():
     cursor2.execute("SELECT * FROM role")
     roledata = cursor2.fetchall()
     cursor2.close()
-    s3 = boto3.resource('s3')
-    return render_template('Staff.html',depart=departdata,role=roledata,staff=staffdata,bucket1=bucket,s31=s3)
+    return render_template('Staff.html',depart=departdata,role=roledata,staff=staffdata)
 
 
 @app.route("/about", methods=['POST'])
