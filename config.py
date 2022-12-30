c65ce41676dff73c1ce7249b267e111754d4f164
JWkahwai <103020993+JWkahwai@users.noreply.github.com>
2022-12-30 22:55:17 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-customhost = "employee.cvizqxos8p8v.us-east-1.rds.amazonaws.com"
+customhost = "staff.cpgv8rvwzl4n.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "staff"
