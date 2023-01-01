984e6348d4dc2affa55477c60826cb1c7e71527f
JWkahwai <103020993+JWkahwai@users.noreply.github.com>
2023-01-02 02:54:42 +0800

    Update StaffApp.py

--- StaffApp.py
+++ StaffApp.py
@@ -27,8 +27,7 @@ try:
     cursor = db_conn.cursor()
     cursor.execute("SELECT FLOOR(5 + RAND()*(10 - 5 + 1)) AS Random_Number")
     temp = cursor.fetchall()
-    for row in temp:
-        getNumber = row["Random_Number"]
+    getNumber = row[0]
     cursor.close()
     
 except pymysql.OperationalError:
@@ -36,8 +35,7 @@ except pymysql.OperationalError:
     cursor = db_conn.cursor()
     cursor.execute("SELECT FLOOR(5 + RAND()*(10 - 5 + 1)) AS Random_Number")
     temp = cursor.fetchall()
-    for row in temp:
-        getNumber = row["Random_Number"]
+    getNumber = row[0]
     cursor.close()
     
 @app.route("/", methods=['GET', 'POST'])
